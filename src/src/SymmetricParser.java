cb62f3025183def5d30a15ecf8d58b06977f7bf7
sunayana gupta <2015uee1754@mnit.ac.in>
2022-10-05 20:12:50 -0700

    Adding symmetric parser and algo

--- src/src/SymmetricParser.java
+++ src/src/SymmetricParser.java
@@ -1,12 +1,72 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class SymmetricParser implements IParser {
 
+    int dimension;
+    Coordinate[] coordinates;
+    Scanner scanner;
     @Override
     public ITsp parse(String path) {
-        List<int[]> distances = new ArrayList<int[]>();
+        setScanner(path);
+        getDimension();
+        return getCoordinates();
+    }
 
+    public void setScanner(String path) {
+        File file = new File(path);
+
+        try {
+            scanner = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void getDimension(){
+        String fileType;
+        String nextLine = scanner.nextLine();
+
+        while (!nextLine.startsWith("DIMENSION")) {
+            nextLine = scanner.nextLine();
+        }
+
+        dimension = Integer.parseInt(nextLine.split(" ")[2]);//use 4th
+        fileType = scanner.nextLine().split(" ")[2];
+        scanner.nextLine();
+    }
+    public ITsp getCoordinates(){
+        int i = 0;
+        coordinates= new Coordinate[dimension];
+
+        while (i < dimension) { // picking exactly the required number of items.
+            String line = scanner.nextLine();
+            String[] values = line.split(" ");
+            double x = Double.parseDouble(values[1]);
+            double y = Double.parseDouble(values[2]);
+            Coordinate coordinate = new Coordinate(x, y);
+            coordinates[i++] = coordinate;
+        }
+        return createAdjacencyMatrix();
+    }
+
+    public ITsp createAdjacencyMatrix() {
+        int[][] distances = new int[dimension][dimension];
+
+        for (int i = 0; i < dimension; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (i == j)
+                    distances[i][j] = 0;
+                else {
+                    double dX = coordinates[i].getX() - coordinates[j].getX();
+                    double dY = coordinates[i].getY() - coordinates[j].getY();
+                    distances[i][j] = (int) Math.round(Math.sqrt(dX * dX + dY * dY));
+                    distances[j][i] = distances[i][j];
+                }
+            }
+        }
         return new SymmetricTsp(distances);
     }
 }
+
--- src/src/SymmetricTsp.java
+++ src/src/SymmetricTsp.java
@@ -3,14 +3,58 @@ import java.util.List;
 
 public class SymmetricTsp implements ITsp{
 
-    private List<int[]> distances = new ArrayList<int[]>();
+    private int[][] distances ;
 
-    public SymmetricTsp(List<int[]> _dist){
+    public SymmetricTsp(int[][] _dist){
         distances = _dist;
     }
 
-    @Override
-    public void execute() {
+        public void findMinRoute() {
+        int sum = 0;
+        int counter = 0;
+        int j = 0, i = 0;
+        int min = Integer.MAX_VALUE;
+        List<Integer> visitedRouteList = new ArrayList<>();
 
+        // Starting from the 0th indexed city i.e., the first city
+        visitedRouteList.add(0);
+        int[] route = new int[distances.length];
+
+        // Traverse the adjacency matrix tsp[][]
+        while (i < distances.length && j < distances[i].length) {
+
+            // Corner of the Matrix
+            if (counter >= distances[i].length - 1) {
+                break;
+            }
+
+            // If this path is unvisited then and if the cost is less than update the cost
+            if (j != i && !(visitedRouteList.contains(j))) {
+                if (distances[i][j] < min) {
+                    min = distances[i][j];
+                    route[counter] = j + 1;
+                }
+            }
+            j++;
+
+            // Check all paths from the ith indexed city
+            if (j == distances[i].length) {
+                sum += min;
+                min = Integer.MAX_VALUE;
+                visitedRouteList.add(route[counter] - 1);
+                j = 0;
+                i = route[counter] - 1;
+                counter++;
+            }
+        }
+
+        i = route[counter - 1] - 1;
+        sum += distances[i][0];
+        route[counter]= 1;
+
+        System.out.print("Minimum Cost is : ");
+        System.out.println(sum);
+        for(int k=0;k<route.length;k++)
+            System.out.print(route[k] + " ");
     }
 }
