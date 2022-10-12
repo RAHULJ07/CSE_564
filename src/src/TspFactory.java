92076d2a029a68dca6603a98e044545cad648ec2
sunayana gupta <2015uee1754@mnit.ac.in>
2022-10-11 22:41:56 -0700

    Adding plotting UI for symmetric TSP cities

--- src/src/TspFactory.java
+++ src/src/TspFactory.java
@@ -2,10 +2,6 @@ public class TspFactory {
 
     static IParser parser;
 
-    public static IParser getTspParserObj(){
-        return parser;
-    }
-
     public static ITsp getTspObj(String path, TspType type){
 
         parser = ParserFactory.getParser(type);
