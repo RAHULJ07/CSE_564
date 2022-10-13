7a6bd89ba6c11d8291c2abcce740849e61eb8fcd
chrislazar25 <89318462+chrislazar25@users.noreply.github.com>
2022-10-13 14:48:52 -0700

    Update Main.java
    -add javadoc
    -display gui from main(setVisible)

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,6 +1,20 @@
+import javax.swing.JFrame;
 
+/**
+ * Main class to instantiate the graphic user interface.
+ * @version 1.0.0.
+ */
 public class Main {
-    public static void main(String args[]) {
-    	new GUI();
+
+	/**
+    * Main method to display window using JFrame.
+    */
+	public static void main(String args[]) {
+       
+    	GUI myGUI = new GUI();
+    	myGUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	myGUI.setSize(1000,1000);
+    	myGUI.setVisible(true);
+				
     }
 }
